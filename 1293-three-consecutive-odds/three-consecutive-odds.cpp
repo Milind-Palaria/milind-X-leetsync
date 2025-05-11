7ff0bef09179d6599966b362291afdd791695892
Milind Palaria <96460537+Milind-Palaria@users.noreply.github.com>
2025-05-11 23:01:23 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (20.63%) - LeetSync

---
+++ 1293-three-consecutive-odds/three-consecutive-odds.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int count=0;
+        int n=arr.size();
+        if(n<3)
+            return false;
+        for(int i=2;i<n;i++){
+            if((arr[i-2]&1) && (arr[i-1]&1) && (arr[i]&1))
+                return true;
+        }
+        return false;
+    }
+};
