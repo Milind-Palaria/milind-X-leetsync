f2d0cbec6488289e9626d461a6e99f37f7b31d47
Milind Palaria <96460537+Milind-Palaria@users.noreply.github.com>
2025-06-16 23:58:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (56.36%) - LeetSync

--- 2704-maximum-difference-by-remapping-a-digit/maximum-difference-by-remapping-a-digit.cpp
+++ 2704-maximum-difference-by-remapping-a-digit/maximum-difference-by-remapping-a-digit.cpp
@@ -2,29 +2,38 @@ class Solution {
 public:
     int minMaxDifference(int num) {
         string s = to_string(num);
-        string maxStr = s;
-        for (char c : s) {
-            if (c != '9') {
-                for (char &ch : maxStr) {
-                    if (ch == c) ch = '9';
-                }
+        int i = 0;
+        char c = '-1';
+        while(i<s.size()){
+            if((s[i]-'0')<9) {
+                c = s[i];
                 break;
             }
+            i++;
         }
-
-        string minStr = s;
-        for (char c : s) {
-            if (c != '0') {
-                for (char &ch : minStr) {
-                    if (ch == c) ch = '0';
-                }
+        string a = s;
+        while(i<a.size()){
+            if(a[i]==c){
+                a[i] = '9';
+            }
+            i++;
+        }
+        char d = '-1';
+        int j = 0;
+        while(j<s.size()){
+            if((s[j]-'0')>0) {
+                d = s[j];
                 break;
             }
+            j++;
         }
-
-        int maxVal = stoi(maxStr);
-        int minVal = stoi(minStr);
-
-        return maxVal - minVal;
+        string b = s;
+        while(j<b.size()){
+            if(b[j]==d){
+                b[j] = '0';
+            }
+            j++;
+        }
+        return stoi(a)-stoi(b);
     }
 };
