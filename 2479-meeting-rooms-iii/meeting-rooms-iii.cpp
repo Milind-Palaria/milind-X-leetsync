0ab71e661de13abb2e1aabaea5aafa2f30b5e824
Milind Palaria <96460537+Milind-Palaria@users.noreply.github.com>
2025-07-11 22:34:37 +0530

    Time: 63 ms (94.43%) | Memory: 98.1 MB (75.53%) - LeetSync

--- 2479-meeting-rooms-iii/meeting-rooms-iii.cpp
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.cpp
@@ -1,70 +1,44 @@
-#define ll long long
 class Solution {
-    unordered_map<int, int> map;  // keeping count of usage of a room
-    priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;    
-// min heap to store endtime of a room and room number
-    priority_queue<int, vector<int>, greater<int>> pqq; // min heap to store available rooms
 public:
-    int mostBooked(int n, vector<vector<int>>& meetings) {
-        sort(meetings.begin(), meetings.end()); // sort so meetings are arranged with increasing start times
-        for(int i=0; i<n; i++) pqq.push(i);
-        for(int i=0; i<meetings.size(); i++) {
-            long long start= meetings[i][0];
-            long long end= meetings[i][1];
-            long long duration= end- start;
-            if(!pqq.empty()) {
-                //rooms are available now
-                while(!pq.empty()) {
-                    if(pq.top().first <= start) {
-                        pqq.push(pq.top().second);
-                        pq.pop();
-// take away those rooms whose meetings have ended on or before startTime
-                    }
-                    else break;
-                }
-                int room= pqq.top();
-                pqq.pop();
-                pq.push({end, room});
-                cout<<end<<" "<<room<<endl;
-                map[room]++;
-// room available so start the meeting
+    #define p pair<ll,int>
+    #define ll long long int
+    int mostBooked(int n, vector<vector<int>>& m) {
+        vector<int> c(n,0);
+        sort(m.begin(),m.end());
+        priority_queue<int, vector<int>, greater<int> >unused;
+        priority_queue<p, vector<p>, greater<p>>pq;
+        for (int i=0;i<n;i++)
+            unused.push(i);
+        ll t=0,s=m.size();
+        for(int i=0;i<s;i++)
+        {
+            t=max(t,(ll)m[i][0]);
+            while(pq.size()>0 && pq.top().first <= t)
+            {
+                unused.push(pq.top().second);
+                pq.pop();
             }
-            else {
-                if(pq.top().first >= start) {
-                    //meeting will be delayed as all rooms are busy
-                    long long newstart= pq.top().first;
-                    int room= pq.top().second;
-                    pq.pop();
-                    pq.push({newstart+duration, room});
-// fixing new start time
-                    cout<<newstart+end-start<<" "<<room<<endl;
-                    map[room]++;
-                }
-                else {
-                    //meeting by default is late scheduled so just clean all records upto the case when meeting can be still scheduled on time i.e. in order to get lowest possible room number from those rooms whose meeting will end before this one's start time.
-                    while(!pq.empty()) {
-                        if(pq.top().first <= start) {
-                            pqq.push(pq.top().second);
-                            pq.pop();
-                        }
-                        else break;
-                    }
-                    int room= pqq.top();
-                    pqq.pop();
-                    pq.push({end, room});
-                    cout<<end<<" "<<room<<endl;
-                    map[room]++;
-                }
+            if(unused.size()==0)
+            {
+                unused.push(pq.top().second);
+                t=pq.top().first;
+                pq.pop();
+            }
+            ll d = m[i][1]-m[i][0];
+            int r = unused.top();
+            c[r]++;
+            unused.pop();
+            pq.push({t+d, r});
+        }
+        int res=0,x=0;
+        for(int i=0;i<n;i++)
+        {
+            if(c[i]>x)
+            {
+                x=c[i];
+                res=i;
             }
         }
-        int maxroom= -1;
-        int maxfreq= 0;
-        for(int i=0; i<n; i++) {
-            int freq= map[i];
-            cout<<"room: "<<i<<" freq: "<<freq<<endl;
-            if(freq>maxfreq) { maxroom= i; maxfreq= freq; }
-//simple comparison for final answer, i hope u won't get stuck here :)
-        }
-        return maxroom;
+        return res;
     }
 };
