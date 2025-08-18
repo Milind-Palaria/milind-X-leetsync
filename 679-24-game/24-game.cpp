c44626060630ae3079ccde8b774e5e0fd823307f
Milind Palaria <96460537+Milind-Palaria@users.noreply.github.com>
2025-08-18 22:33:30 +0530

    Time: 9 ms (77.75%) | Memory: 12.3 MB (83.92%) - LeetSync

---
+++ 679-24-game/24-game.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool judgePoint24(vector<int>& cards) {
+        vector<double> nums;
+        for (int num : cards)
+            nums.push_back(num);
+        return solve(nums);
+    }
+    
+    bool solve(vector<double>& nums) {
+        if (nums.size() == 1)
+            return fabs(nums[0] - 24) < 1e-6;
+        
+        for (int i = 0; i < nums.size(); ++i) {
+            for (int j = 0; j < nums.size(); ++j) {
+                if (i == j) continue;
+                vector<double> next;
+                for (int k = 0; k < nums.size(); ++k) {
+                    if (k != i && k != j)
+                        next.push_back(nums[k]);
+                }
+                for (int op = 0; op < 4; ++op) {
+                    if (op < 2 && i > j)
+                        continue;
+                    double res = 0;
+                    if (op == 0) res = nums[i] + nums[j];
+                    if (op == 1) res = nums[i] * nums[j];
+                    if (op == 2) res = nums[i] - nums[j];
+                    if (op == 3) {
+                        if (fabs(nums[j]) < 1e-6) continue;
+                        res = nums[i] / nums[j];
+                    }
+                    next.push_back(res);
+                    if (solve(next)) return true;
+                    next.pop_back(); 
+                }
+            }
+        }
+        return false;
+    }
+};
